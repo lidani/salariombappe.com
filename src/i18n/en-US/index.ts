3307af7088626e57158a4c22721f758080c2b221
Gustavo Lidani <gustavo@lidani.dev>
2023-07-24 19:48:31 -0300

    i18n keys

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,4 +1,11 @@
 export default {
   GoHomeDescription: 'Go Home',
   NothingHere: 'Oops. Nothing here...',
+
+  DisplayNameWithSalary: `${import.meta.env.VITE_APP_PLAYER_NAME}'s Salary`,
+  SinceYouEntered: 'Since you entered this page',
+  SinceBefore: '',
+  SinceAfter: 'ago',
+  Won: 'won',
+  Or: 'or',
 };
--- src/i18n/pt-BR/index.ts
+++ src/i18n/pt-BR/index.ts
@@ -1,4 +1,11 @@
 export default {
   GoHomeDescription: 'Voltar para o início',
   NothingHere: 'Ops! Nada por aqui...',
+
+  DisplayNameWithSalary: `Salário do ${import.meta.env.VITE_APP_PLAYER_NAME}`,
+  SinceYouEntered: 'Desde que você entrou nesta página',
+  SinceBefore: 'há',
+  SinceAfter: 'atrás',
+  Won: 'ganhou',
+  Or: 'ou',
 };

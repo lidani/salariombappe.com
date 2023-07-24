aa6f34b54741cf3a50ffe8428389cc0f8703fa2b
Gustavo Lidani <gustavo@lidani.dev>
2023-07-24 19:50:57 -0300

    github link and choose lang

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,30 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-page-container>
+    <q-header class="text-black" style="background-color: transparent">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="mdi-github"
+          aria-label="Menu"
+          @click="onClickGithub"
+        />
+
+        <q-select
+          v-model="locale"
+          :options="localeOptions"
+          dense
+          class="q-ml-auto"
+          borderless
+          emit-value
+          map-options
+          options-dense
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="q-pa-sm text-center">
       <router-view />
     </q-page-container>
   </q-layout>
@@ -8,8 +32,24 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
   name: 'MainLayout',
+  setup() {
+    const onClickGithub = () =>
+      window.open('https://github.com/lidani/salariombappe.com', '_blank');
+
+    const { locale } = useI18n({ useScope: 'global' });
+
+    return {
+      locale,
+      localeOptions: [
+        { value: 'en-US', label: 'English' },
+        { value: 'pt-BR', label: 'Português' },
+      ],
+      onClickGithub,
+    };
+  },
 });
 </script>
